b58d349aecc1bb9ece8bfee020f5b6e05f7a55f7
obrandemburg <gdmatheus12@gmail.com>
2025-06-25 15:02:30 -0300

    Versão base, sem otimização

---
+++ SIMDRPG/Personagem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+
+public struct Personagem
+{
+    public int Ataque;
+    public int Defesa;
+    public int ChanceCritico; // 0-100
+    public int MultCritico;   // multiplicador de crítico (ex: 200 = 2x)
+    public int Vida;
+    public bool Vivo;
+}
